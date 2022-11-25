abd838429da6ec001b878df573195f25f9c127a8
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-11-25 20:05:05 +0200

    refactoring express server for using socketio

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 
 const app = express();
+// creating server with http to pass it for socketio function
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 
@@ -9,6 +14,11 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+// using on connection event from socketio
+io.on('connection', () => {
+    console.log('New WebSocket connection');
+});
+
+server.listen(port, () => {
     console.log(`server is up on port: ${port}`);
 });

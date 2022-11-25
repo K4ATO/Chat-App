cfca7e64044e743e766fa4104b74418204eea61c
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-11-25 23:17:02 +0200

    add form status

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,29 @@
 const socket = io();
 
+// elements
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $shareLocationButton = document.querySelector('#share-location');
 // event listener for receiving a message
 socket.on('message', (message) => {
     console.log(message);
 });
 
 // event listener for clients messages
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    // disable the form
+    $messageFormButton.setAttribute('disabled', 'disabled');
+
     const message = e.target.elements.message.value;
     socket.emit('sendMessage', message, (error) => {
+        // enable the form
+        $messageFormButton.removeAttribute('disabled');
+        // resetting the value of the message input field
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
         if (error) {
             return console.log(error);
         }
@@ -18,11 +32,11 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 });
 
 // event listener for sharing users location
-document.querySelector('#share-location').addEventListener('click', () => {
+$shareLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser');
     }
-
+    $shareLocationButton.setAttribute('disabled', 'disabled');
     navigator.geolocation.getCurrentPosition((position) => {
         const userLocation = {
             latitude: position.coords.latitude,
@@ -30,6 +44,7 @@ document.querySelector('#share-location').addEventListener('click', () => {
         };
         socket.emit('shareLocation', userLocation, () => {
             console.log('Location shared.');
+            $shareLocationButton.removeAttribute('disabled');
         });
     });
 });

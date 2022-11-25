87c269cc396f0dcc87f750a05793cfc05a383df6
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-11-25 20:44:27 +0200

    add sending message, joining users, deleting users

--- src/index.js
+++ src/index.js
@@ -15,8 +15,18 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
 // using on connection event from socketio
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('New WebSocket connection');
+    socket.emit('message', 'Welcome!');
+
+    socket.broadcast.emit('message', 'A new user has joined!');
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message);
+    });
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!');
+    });
 });
 
 server.listen(port, () => {

c414fa7f19810e60f163b5589a93e7394cec5af4
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-11-26 13:40:15 +0200

    add user utils

---
+++ src/utils/users.js
@@ -0,0 +1,59 @@
+const users = [];
+
+// get user
+const getUser = (id) => {
+    return users.find((user) => user.id === id);
+};
+
+// get users
+const getUsers = (room) => {
+    room = room.trim().toLowerCase();
+    return users.filter((user) => user.room === room);
+};
+
+// add user
+const addUser = ({ id, username, room }) => {
+    // Clean the data
+    username = username.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    // Validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required.',
+        };
+    }
+
+    // Check for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username;
+    });
+
+    // Validate username
+    if (existingUser) {
+        return {
+            error: 'Username is in use.',
+        };
+    }
+
+    // Store user
+    const user = { id, username, room };
+    users.push(user);
+    return { user };
+};
+
+// remove user
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id);
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0];
+    }
+};
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsers,
+};
